ec7e0ca4179dca7b35cecde639d19d021a1b8a13
sumangitron <sumanabatar1994@gmail.com>
2024-03-07 13:23:43 +0530

    google sheet

---
+++ data-flow.js
@@ -0,0 +1,178 @@
+const selectedCellElement = document.querySelector(".selected-cell");
+const form = document.querySelector("form");
+
+let totalSheet = 1;
+let activeSheetName = "sheet1";
+
+let state = {};
+
+const defultStyle = {
+  fontFamily: "roboto",
+  fontSize: 16,
+  bold: false,
+  italic: false,
+  underline: false,
+  align: "left",
+  bgColor: "#ffffff",
+  textColor: "#000000",
+  // content: "",
+};
+
+function applyStylesToElement(element, styles) {
+  element.style.fontFamily = styles.fontFamily;
+  element.style.fontSize = `${styles.fontSize}px`;
+  element.style.textAlign = styles.align;
+  element.style.fontWeight = styles.bold
+    ? "bold"
+    : "normal";
+  element.style.fontStyle = styles.italic
+    ? "italic"
+    : "normal";
+  element.style.textDecoration = styles.underline
+    ? "underline"
+    : "none";
+  element.style.color = styles.textColor;
+  element.style.backgroundColor = styles.bgColor;
+}
+
+form.addEventListener("change", () => {
+  const selectedValues = {
+    fontFamily: form.fontFamily.value,
+    fontSize: Number(form.fontSize.value),
+    bold: form.bold.checked,
+    italic: form.italic.checked,
+    underline: form.underline.checked,
+    align: form.align.value,
+    bgColor: form.bgColor.value,
+    textColor: form.textColor.value,
+  };
+
+  //apply the styles to the selected cell
+  const selectedCellElement = document.getElementById(selectedCell);
+  applyStylesToElement(selectedCellElement, selectedValues);
+
+  // selectedCellElement.style.fontFamily = selectedValues.fontFamily;
+  // selectedCellElement.style.fontSize = `${selectedValues.fontSize}px`;
+  // selectedCellElement.style.textAlign = selectedValues.align;
+  // selectedCellElement.style.fontWeight = selectedValues.bold
+  //   ? "bold"
+  //   : "normal";
+  // selectedCellElement.style.fontStyle = selectedValues.italic
+  //   ? "italic"
+  //   : "normal";
+  // selectedCellElement.style.textDecoration = selectedValues.underline
+  //   ? "underline"
+  //   : "none";
+  // selectedCellElement.style.color = selectedValues.textColor;
+  // selectedCellElement.style.backgroundColor = selectedValues.bgColor;
+
+  state[selectedCell] = selectedValues;
+});
+
+let selectedCell = null;
+
+function onCellFocus(event) {
+  if (selectedCell) {
+    //remove the active-cell class for the previously focused cell
+    document.getElementById(selectedCell).classList.remove("active-cell");
+  }
+  selectedCell = event.target.id;
+  selectedCellElement.innerText = selectedCell;
+  if (!state[selectedCell]) {
+    //when the cell is focused for the first time
+    state[selectedCell] = defultStyle;
+  }
+
+  //add active-cell class for newly focused element
+  document.getElementById(selectedCell).classList.add("active-cell");
+
+  applyCurrentCellStyleToForm();
+}
+
+function applyCurrentCellStyleToForm() {
+  //apply style of the current selected cell to the form
+  form.bold.checked = state[selectedCell].bold;
+  form.italic.checked = state[selectedCell].italic;
+  form.underline.checked = state[selectedCell].underline;
+
+  form.fontFamily.value = state[selectedCell].fontFamily;
+  form.fontSize.value = state[selectedCell].fontSize;
+  form.align.value = state[selectedCell].align;
+  form.textColor.value = state[selectedCell].textColor;
+  form.bgColor.value = state[selectedCell].bgColor;
+}
+
+// function onChangeContent() {
+//   const clickedCell = document.getElementById(selectedCell);
+//   let contentValue = clickedCell.innerText;
+//   console.log(contentValue);
+//   state[selectedCell].content = contentValue;
+// }
+
+//expression evalution
+const fx = document.getElementById("fx");
+fx.addEventListener("keyup", (event) => {
+  if(!selectedCell) {
+    alert("Please select any cell");
+    fx.value = "";
+  }
+  if (event.code === "Enter" && selectedCell) {
+    let expression = fx.value;
+    let result = eval(expression);
+    document.getElementById(selectedCell).innerText = result;
+    fx.value = "";
+  }
+});
+
+//new sheet flow
+
+const footForm = document.querySelector(".foot-form");
+
+footForm.addEventListener("change", (event) => {
+  let newSheetName = event.target.value;
+  //save the current sheet into local storage
+  localStorage.setItem(activeSheetName, JSON.stringify(state));
+  //clear all the cells effected/edited in the current sheet
+  for(let cellid in state) {
+    clearCell(cellid);
+  }
+  //reset the state object
+  let existingData = localStorage.getItem(newSheetName);
+
+  if(existingData) {
+    state = JSON.parse(existingData);
+    //TODO: apply the styles to all the individual cell present inside the existing data
+    for(let cellId in state) {
+      const cellElement = document.getElementById(cellId);
+      applyStylesToElement(cellElement, state[cellId]);
+    }
+  }
+  else {
+    //as there is no existing data
+    state = {};
+  }
+
+  //update the active sheet name to be the selected one
+  activeSheetName = newSheetName; 
+})
+
+function creatNewSheet() {
+  totalSheet++;
+
+  let newSheetName = `Sheet${totalSheet}`;
+
+  const inputContainer = document.createElement("div");
+  inputContainer.innerHTML = `
+          <input type="radio" name="sheet" id="${newSheetName}" value = "${newSheetName}">
+          <label for="${newSheetName}">${newSheetName}</label>
+  `;
+
+  footForm.appendChild(inputContainer);
+}
+
+function clearCell(cellId) {
+  let cell = document.getElementById(cellId);
+  cell.innerText = "";
+  cell.removeAttribute("style");
+  cell.classList.remove("active-cell");
+}
---
+++ script.js
@@ -0,0 +1,47 @@
+let header = document.querySelector(".header");
+let snoContainer = document.querySelector(".sno-container");
+let rowsContainer = document.querySelector(".rows-container");
+
+let columns = 27;
+let rows = 50;
+
+//creating header cells from A to Z
+for(let i=0; i<columns; i++) {
+    let cell = document.createElement("div");
+    i === 0 ? (cell.className = "first-cell") : (cell.className = "cell");
+    if(i != 0) {
+        cell.innerText = String.fromCharCode(64 + i);
+    }
+    header.appendChild(cell);
+}
+
+//function for creat rows for rows container
+function creatRow(rowNumber) {
+    let row = document.createElement("div");
+    row.className = "row";
+
+    for(let i=1; i<columns; i++) {
+        let cell = document.createElement("div");
+        cell.className = "cell";
+        cell.contentEditable = true;
+        cell.id = `${String.fromCharCode(64 + i)}${rowNumber}`;
+        cell.addEventListener("focus", onCellFocus);
+        // cell.addEventListener("input", onChangeContent);
+        row.appendChild(cell);
+    }
+
+    return row;
+}
+
+//loop for create serial no rows from 1 to 50 and rows inside rows container
+for(let i=1; i<=rows; i++) {
+    let snoCell = document.createElement("div")
+    snoCell.innerText = i;
+    snoCell.className = "sno";
+    snoContainer.appendChild(snoCell);
+
+    let rowElement = creatRow(i);
+
+    rowsContainer.appendChild(rowElement);
+
+}
